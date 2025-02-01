91b47ba6c34f49f4b33b6a090039aa60cba194c6
sheepdog0351 <michaelwork2006@gmail.com>
2025-02-01 18:54:47 -0500

    finished working on structs

---
+++ 3_custom_types/target/debug/deps/custom_types-bdb210faa0f2e63d.d
@@ -0,0 +1,5 @@
+C:\rust_by_example\3_custom_types\target\debug\deps\libcustom_types-bdb210faa0f2e63d.rmeta: src\main.rs
+
+C:\rust_by_example\3_custom_types\target\debug\deps\custom_types-bdb210faa0f2e63d.d: src\main.rs
+
+src\main.rs:
---
+++ 3_custom_types/target/debug/deps/custom_types-dbc13e2f88532071.d
@@ -0,0 +1,5 @@
+C:\rust_by_example\3_custom_types\target\debug\deps\libcustom_types-dbc13e2f88532071.rmeta: src\main.rs
+
+C:\rust_by_example\3_custom_types\target\debug\deps\custom_types-dbc13e2f88532071.d: src\main.rs
+
+src\main.rs:
